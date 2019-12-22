cceb002a5592cdea35b5109314bc92150b9e2f81
Marcos <markettes_99@outlook.es>
2019-12-22 10:24:19 +0100

    gravity not working

--- app/src/main/java/devcom/marcos/accelerometerprinter/MainActivity.java
+++ app/src/main/java/devcom/marcos/accelerometerprinter/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView tf;
     private double[] gravity = new double[3];
     private double[] linear_acceleration = new double[3];
+    private Sensor mSensorG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,38 +27,57 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         tf = findViewById(R.id.tf);
         mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
         mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mSensorG = mSensorManager.getDefaultSensor(Sensor.TYPE_GRAVITY);
     }
 
     protected void onResume() {
         super.onResume();
         mSensorManager.registerListener(this, mSensor, SensorManager.SENSOR_DELAY_FASTEST);
+        mSensorManager.registerListener(this, mSensorG, SensorManager.SENSOR_DELAY_FASTEST);
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this);
+        mSensorManager.unregisterListener(this, mSensor);
+        mSensorManager.unregisterListener(this, mSensorG);
     }
 
     public void onSensorChanged(SensorEvent var1){
-        // In this example, alpha is calculated as t / (t + dT),
-        // where t is the low-pass filter's time-constant and
-        // dT is the var1 delivery rate.
 
-        final double alpha = 0.8;
+        switch (var1.sensor.getType()){
+            case Sensor.TYPE_ACCELEROMETER:
+                /*
+                // In this example, alpha is calculated as t / (t + dT),
+                // where t is the low-pass filter's time-constant and
+                // dT is the var1 delivery rate.
 
-        // Isolate the force of gravity with the low-pass filter.
-        gravity[0] = alpha * gravity[0] + (1 - alpha) * var1.values[0];
-        gravity[1] = alpha * gravity[1] + (1 - alpha) * var1.values[1];
-        gravity[2] = alpha * gravity[2] + (1 - alpha) * var1.values[2];
+                final double alpha = 0.8;
 
-        // Remove the gravity contribution with the high-pass filter.
-        linear_acceleration[0] = var1.values[0] - gravity[0];
-        linear_acceleration[1] = var1.values[1] - gravity[1];
-        linear_acceleration[2] = var1.values[2] - gravity[2];
-        Log.i("x", Float.toString(var1.values[0]));
-        Log.i("y", Float.toString(var1.values[1]));
-        Log.i("z", Float.toString(var1.values[2]));
-        tf.setText(Double.toString(linear_acceleration[0])+" "+Double.toString(linear_acceleration[1])+" "+Double.toString(linear_acceleration[2]));
+                // Isolate the force of gravity with the low-pass filter.
+                gravity[0] = alpha * gravity[0] + (1 - alpha) * var1.values[0];
+                gravity[1] = alpha * gravity[1] + (1 - alpha) * var1.values[1];
+                gravity[2] = alpha * gravity[2] + (1 - alpha) * var1.values[2];
+
+                // Remove the gravity contribution with the high-pass filter.
+                linear_acceleration[0] = var1.values[0] - gravity[0];
+                linear_acceleration[1] = var1.values[1] - gravity[1];
+                linear_acceleration[2] = var1.values[2] - gravity[2];
+                Log.i("x", Float.toString(var1.values[0]));
+                Log.i("y", Float.toString(var1.values[1]));
+                Log.i("z", Float.toString(var1.values[2]));
+                tf.setText(Double.toString(linear_acceleration[0])+" "+Double.toString(linear_acceleration[1])+" "+Double.toString(linear_acceleration[2]));
+                */
+                Log.i("x", Double.toString(var1.values[0] - gravity[0]));
+                Log.i("y", Double.toString(var1.values[1] - gravity[1]));
+                Log.i("z", Double.toString(var1.values[2] - gravity[2]));
+                break;
+
+            case Sensor.TYPE_GRAVITY:
+                gravity[0] = var1.values[0];
+                gravity[1] = var1.values[1];
+                gravity[2] = var1.values[2];
+                break;
+        }
     }
 
     public void onAccuracyChanged(Sensor sensor, int accuracy){
