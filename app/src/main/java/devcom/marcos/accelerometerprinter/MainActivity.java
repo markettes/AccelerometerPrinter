f7f626ad0e08e02576c6a9ef3df7eb0e84904401
Marcos <markettes_99@outlook.es>
2020-01-16 17:10:38 +0100

    Limited accuracy

--- app/src/main/java/devcom/marcos/accelerometerprinter/MainActivity.java
+++ app/src/main/java/devcom/marcos/accelerometerprinter/MainActivity.java
@@ -9,6 +9,7 @@ import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.telephony.mbms.MbmsErrors;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,23 +20,34 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     //Maths
     private double time;
+    private double minTime;
+    private double maxTime;
     private double[] distance = new double[3];
+    private double[] vector_rotation = new double[3];
+    private double[] gravity = new double[3];
     private double[] linear_acceleration = new double[3];
+    private double[] max_linear_acceleration = new double[3];
     private double[] speed = new double[3];
 
     //Others
+    boolean first;
     private DecimalFormat f;
+    private DecimalFormat f1;
     private SensorManager mSensorManager;
-    private Sensor mSensor;
+    private Sensor mSensorAcc;
+    private Sensor mSensorRot;
     private SensorEventListener se;
 
     //UI
     private TextView tf;
+    private TextView tf1;
     private TextView tf2;
     private TextView tf3;
     private TextView periodTF;
     private Button startB;
     private Button stopB;
+    private Button startB2;
+    private Button stopB2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,18 +55,22 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         //Initialize Maths
-        for (int i = 0; i<3; i++){
+        for (int i = 0; i < 3; i++) {
             distance[i] = 0;
             linear_acceleration[i] = 0;
             speed[i] = 0;
         }
+        maxTime = -1;
+        minTime = 99999;
         time = System.nanoTime() / 1000000000.0;
 
         //Initialize Others
-        f = new DecimalFormat("##0.00000");
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
+        f = new DecimalFormat("##0.00");
+        f1 = new DecimalFormat("##0.0");
+        mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+        mSensorAcc = mSensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION);
         se = this;
+        first = true;
 
         //Initialize UI
         tf = findViewById(R.id.tf);
@@ -67,50 +83,63 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         startB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mSensorManager.registerListener(se, mSensor, 1000000);
+                mSensorManager.registerListener(se, mSensorAcc, SensorManager.SENSOR_DELAY_UI);
             }
         });
         stopB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mSensorManager.unregisterListener(se, mSensor);
+                mSensorManager.unregisterListener(se, mSensorAcc);
             }
         });
+
     }
 
     protected void onResume() {
         super.onResume();
-        //mSensorManager.registerListener(se, mSensor, SensorManager.SENSOR_DELAY_GAME);
     }
 
     protected void onPause() {
         super.onPause();
-        mSensorManager.unregisterListener(this, mSensor);
+        mSensorManager.unregisterListener(se, mSensorAcc);
     }
 
-    public void onSensorChanged(SensorEvent var1){
-        time = System.nanoTime() / 1000000000.0 - time;
-        periodTF.setText("Period: " + f.format(time) + "s");
-        distance[0] += (speed[0] * time) + (linear_acceleration[0] * time * time) / 2;
-        distance[1] += (speed[1] * time) + (linear_acceleration[1] * time * time) / 2;
-        distance[2] += (speed[2] * time) + (linear_acceleration[2] * time * time) / 2;
+    public void onSensorChanged(SensorEvent var1) {
+        time = Double.parseDouble(f.format(System.nanoTime() / 1000000000.0 - time));
+        if (!first) {
+            for (int i = 0; i < 3; i++) {
+                if (max_linear_acceleration[i] < linear_acceleration[i])
+                    max_linear_acceleration[i] = linear_acceleration[i];
+            }
+            if (time > maxTime) maxTime = time;
+            if (time < minTime) minTime = time;
+        }
+        periodTF.setText("MinTime: " + f.format(minTime) + "s MaxTime: " + f.format(maxTime) + "s Period: " + f.format(time) + "s\n" +
+                "Max linear Acc: " + f.format(max_linear_acceleration[0]) + " " + f.format(max_linear_acceleration[1]) +
+                " " + f.format(max_linear_acceleration[2]));
+        distance[0] += (Double.parseDouble(f1.format(speed[0])) * time) + (linear_acceleration[0] * time * time) / 2;
+        distance[1] += (Double.parseDouble(f1.format(speed[1])) * time) + (linear_acceleration[1] * time * time) / 2;
+        distance[2] += (Double.parseDouble(f1.format(speed[2])) * time) + (linear_acceleration[2] * time * time) / 2;
 
         speed[0] += linear_acceleration[0] * time;
         speed[1] += linear_acceleration[1] * time;
         speed[2] += linear_acceleration[2] * time;
-
+        first = false;
         time = System.nanoTime() / 1000000000.0;
-        linear_acceleration[0] = var1.values[0];
-        linear_acceleration[1] = var1.values[1];
-        linear_acceleration[2] = var1.values[2];
 
-        tf.setText(f.format(linear_acceleration[0])+" "+f.format(linear_acceleration[1])+" "+f.format(linear_acceleration[2]));
-        tf2.setText(f.format(distance[0])+" "+f.format(distance[1])+" "+f.format(distance[2]));
-        tf3.setText(f.format(speed[0])+" "+f.format(speed[1])+" "+f.format(speed[2]));
-        Log.i("info", f.format(linear_acceleration[0])+" "+f.format(linear_acceleration[1])+" "+f.format(linear_acceleration[2]));
+        linear_acceleration[0] = Double.parseDouble(f1.format(var1.values[0]));
+        linear_acceleration[1] = Double.parseDouble(f1.format(var1.values[1]));
+        linear_acceleration[2] = Double.parseDouble(f1.format(var1.values[2]));
+
+
+        tf.setText(f.format(linear_acceleration[0]) + " " + f.format(linear_acceleration[1]) + " " + f.format(linear_acceleration[2]));
+        tf2.setText(f.format(distance[0]) + " " + f.format(distance[1]) + " " + f.format(distance[2]));
+        tf3.setText(f1.format(speed[0]) + " " + f1.format(speed[1]) + " " + f1.format(speed[2]));
+        Log.i("info", f.format(linear_acceleration[0]) + " " + f.format(linear_acceleration[1]) + " " + f.format(linear_acceleration[2]));
     }
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
 
